9ba255fa029350c2f79142ae06bc807f935e2569
MatheusSouzaFaria <aulasfatec2024@gmail.com>
2026-08-25 16:08:04 -0300

    Feature: Card de avisos e Resumo da Grade do professor

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { GradeSidebar } from './grade-sidebar/grade-sidebar';
 
 @Component({
   selector: 'app-root',
-  standalone: false,
-  styleUrl: './app.css',
+  imports: [RouterOutlet, GradeSidebar],
   templateUrl: './app.html',
+  styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('front-avaliability-angular');
+  protected readonly title = signal('Disponibility');
 }
---
+++ src/app/grade-sidebar/grade-sidebar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grade-sidebar',
+  imports: [],
+  templateUrl: './grade-sidebar.html',
+  styleUrl: './grade-sidebar.css',
+})
+export class GradeSidebar {
+
+}
