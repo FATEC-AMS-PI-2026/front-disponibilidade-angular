d65312bcd9f05d700ac66f3f3c6dec6d13b4b995
Arthur Brasil <brasil2403@gmail.com>
2026-08-31 16:22:41 -0300

    Simplify App class by removing signal usage
    Removed unused signal import and property from App class.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,8 @@
-import { Component, signal } from '@angular/core';
-
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   standalone: false,
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
-export class App {
-  protected readonly title = signal('front-avaliability-angular');
-}
+export class App {}
