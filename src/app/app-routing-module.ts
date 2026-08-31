c923ca84f795c64e2a27cae2bee7dabb13843f45
Arthur Brasil <brasil2403@gmail.com>
2026-08-31 16:20:48 -0300

    Add routing for salas and details components

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', redirectTo: 'salas', pathMatch: 'full' },
+  {
+    path: 'salas',
+    loadComponent: () =>
+      import('./features/salas/lista-salas/lista-salas').then((m) => m.ListaSalas),
+  },
+  {
+    path: 'salas/:id',
+    loadComponent: () =>
+      import('./features/salas/detalhes-sala/detalhes-sala').then((m) => m.DetalhesSala),
+  },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
